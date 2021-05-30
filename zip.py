229f0b3e6653dcd5b7f98d90f83168bd072524c3
Amit Tomar <49426082+amittomar-1@users.noreply.github.com>
2021-05-30 07:34:41 +0530

    Update zip.py

--- zip.py
+++ zip.py
@@ -4,6 +4,6 @@ import zipfile
 z = zipfile.ZipFile(sys.argv[1], 'w')
 
 for file in sys.argv[2:]:
-    print("Adding {} to {}").format(file, sys.argv[1])
+    print("Adding {} to {}").format(file, sys.argv[1]) 
     z.write(file)
 
